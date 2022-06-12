bae9eaf4a049e10f88dce0e6a36e67ae835d2396
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-06-12 18:09:33 +0200

    add resource figure

---
+++ src/api/figure/controllers/figure.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  figure controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::figure.figure');
---
+++ src/api/figure/services/figure.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * figure service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::figure.figure');
