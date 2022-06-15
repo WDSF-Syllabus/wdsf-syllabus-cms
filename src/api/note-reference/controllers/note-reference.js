9e55e209276c0d9ced5fbb51ff1be1f575be5368
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-06-15 14:21:07 +0200

    🔥 remove note references

--- src/api/note-reference/controllers/note-reference.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  note-reference controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::note-reference.note-reference');
--- src/api/note-reference/services/note-reference.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * note-reference service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::note-reference.note-reference');
