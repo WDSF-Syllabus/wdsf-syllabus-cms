086a406a2a1bd6c05e791a3f37ccbfeb45fabd55
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-06-15 14:22:18 +0200

    ✨ add foot placement

---
+++ src/api/foot-placement/routes/foot-placement.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * foot-placement router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::foot-placement.foot-placement');
---
+++ src/api/foot-placement/services/foot-placement.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * foot-placement service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::foot-placement.foot-placement');
