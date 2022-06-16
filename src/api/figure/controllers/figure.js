c5e9046781b3256a5de29b8f8efcf288c0abdee3
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-06-16 17:18:05 +0200

    ✨ populate nested figure data

--- src/api/figure/controllers/figure.js
+++ src/api/figure/controllers/figure.js
@@ -12,7 +12,21 @@ module.exports = createCoreController('api::figure.figure', ({ strapi }) =>  ({
 
     const entity = await strapi.db.query('api::figure.figure').findOne({
       where: { slug: id },
-      populate: true,
+      populate: {
+        dance: true,
+        startPositions: true,
+        endPositions: true,
+        timings: true,
+        notes: {
+          populate: {
+            categories: true,
+          }
+        },
+        startFootPlacements: true,
+        endFootPlacements: true,
+        startFoot: true,
+        endFoot: true,
+      },
     });
 
     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
